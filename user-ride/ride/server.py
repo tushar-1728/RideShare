a9be2084ee15b0c02f052317be02c5c6f8df8981
tushar-1728 <tushar-1728@github.com>
2020-05-05 17:56:35 +0530

    basic typos

--- user-ride/ride/server.py
+++ user-ride/ride/server.py
@@ -19,32 +19,27 @@ def addRide():
 	destination = req['destination']
 	timestamp = req['timestamp']
 	if username == '' or username == None or source == '' or source == None or destination == '' or destination == None or timestamp == '' or timestamp == None:
-		print(1)
-		return make_response('',400)
+		return make_response('Any of the 4 given fields is empty.',400)
 	
 	try:
 		datetime.strptime(timestamp,"%d-%m-%Y:%S-%M-%H")
 
 		if datetime.strptime(datetime.now().strftime("%d-%m-%Y:%S-%M-%H"),"%d-%m-%Y:%S-%M-%H") >= datetime.strptime(timestamp,"%d-%m-%Y:%S-%M-%H"):
-			print(2)
-			return make_response('',400)
+			return make_response('Timestamp is invalid.',400)
 
 		user_list = requests.get("http://54.227.171.39:80/api/v1/users", headers={'Origin': '54.227.171.39'}).json()
 		if(username not in user_list):
-			print(3)
-			return make_response('',400)
+			return make_response('User does not exist.',400)
 
 		if requests.get("http://34.238.33.155:80/api/v1/db/read", params={"ORIGIN":"RIDE", "COMMAND":"EXISTS", "FIELD":"Area No", "VALUE":source, "COLLECTION":"Area"}).json()["count"] == 0 or requests.get("http://34.238.33.155:80/api/v1/db/read", params={"ORIGIN":"RIDE", "COMMAND":"EXISTS", "FIELD":"Area No", "VALUE":destination, "COLLECTION":"Area"}).json()["count"] == 0:
-			print(4)
-			return make_response('',400)
+			return make_response('Either source or destination is invalid.',400)
 
 		requests.post("http://34.238.33.155:80/api/v1/db/write", data=json.dumps({"ORIGIN":"RIDE", "COMMAND":"INSERT", "FIELDS":["created_by","users","timestamp","source","destination"], "VALUES":[username,[username],timestamp,source,destination], "COLLECTION":"Rides"}))
 
 		requests.post("http://34.238.33.155:80/api/v1/db/write", data=json.dumps({"ORIGIN":"RIDE", "COMMAND":"ADD_RIDE_COUNT"}))
 		return make_response('',201)
 	except:
-		print(5)
-		return make_response('',400)
+		return make_response('Unknown error',400)
 
 
 # api 4
@@ -54,10 +49,10 @@ def list_rides():
 	source = request.args.get('source')
 	destination = request.args.get('destination')
 	if source == '' or source == None or destination == None or destination == '':
-		return make_response('',400)
+		return make_response('Either source or destination is empty.',400)
 
 	if requests.get("http://34.238.33.155:80/api/v1/db/read", params={"ORIGIN":"RIDE", "COMMAND":"EXISTS", "FIELD":"Area No", "VALUE":source, "COLLECTION":"Area"}).json()["count"] == 0 or requests.get("http://34.238.33.155:80/api/v1/db/read", params={"ORIGIN":"RIDE", "COMMAND":"EXISTS", "FIELD":"Area No", "VALUE":destination, "COLLECTION":"Area"}).json()["count"] == 0:
-			return make_response('',400)
+			return make_response('Either source or destination is invalid.',400)
 
 	res = requests.get('http://34.238.33.155:80/api/v1/db/read',params={"ORIGIN":"RIDE", 'COMMAND':'Upcoming','source':source,'destination':destination})
 	if res.status_code == 204:
@@ -71,10 +66,10 @@ def details_ride(rideid):
 	requests.post("http://34.238.33.155:80/api/v1/db/write", data=json.dumps({"ORIGIN":"RIDE", "COMMAND":"ADD_REQUEST_COUNT"}))
 	message = {}
 	if rideid == '' or rideid == None:
-		return make_response('',400)
+		return make_response('Rideid is empty.',400)
 
 	if requests.get("http://34.238.33.155:80/api/v1/db/read", params={"ORIGIN":"RIDE", "COMMAND":"EXISTS", "FIELD":"_id", "VALUE":rideid, "COLLECTION":"Rides"}).json()["count"] == 0:
-		return make_response('',400)
+		return make_response('Rideid is invalid.',400)
 
 	message = requests.get('http://34.238.33.155:80/api/v1/db/read',params={"ORIGIN":"RIDE", 'COMMAND':'Ride_Details','id':rideid}).json()
 	message = dict(message)
@@ -91,30 +86,27 @@ def Join_ride(rideid):
 	if req == None:
 		return make_response('',400)
 	if 'username' not in req:
-		return make_response(jsonify(''),400)
+		return make_response(jsonify('Request does not consist of username.'),400)
 
 	username = req["username"]
 
 	if username == '' or username == None or rideid == '' or rideid == None:
-		print(1)
-		return make_response('',400)
+		return make_response('username name is empty.',400)
 
 	if requests.get("http://34.238.33.155:80/api/v1/db/read", params={"ORIGIN":"RIDE", "COMMAND":"EXISTS", "FIELD":"_id", "VALUE":rideid, "COLLECTION":"Rides"}).json()["count"] == 0:
-		print(2)
-		return make_response('',400)
+		return make_response('RideID does not exist.',400)
 
 	user_list = requests.get("http://54.227.171.39:80/api/v1/users",headers={'Origin': '54.227.171.39'}).json()
 	if(username not in user_list):
-		print(3)
 		print(user_list)
-		return make_response('',400)
+		return make_response('username does not exist.',400)
 
 	message = requests.get('http://34.238.33.155:80/api/v1/db/read',params={"ORIGIN":"RIDE", 'COMMAND':'Ride_Details','id':int(rideid)}).json()
 	message = dict(message)
 	if(username in message["users"]):
-		return make_response("", 400)
+		return make_response("creator of a ride can not join his own ride.", 400)
 	if datetime.strptime(datetime.now().strftime("%d-%m-%Y:%S-%M-%H"),"%d-%m-%Y:%S-%M-%H") > datetime.strptime(message["timestamp"],"%d-%m-%Y:%S-%M-%H"):
-		return make_response("", 400)
+		return make_response("check time-stamp", 400)
 
 	requests.post("http://34.238.33.155:80/api/v1/db/write", data=json.dumps({"ORIGIN":"RIDE", "COMMAND":"Update_Ride", "id":int(rideid), "username":username}))
 
@@ -126,10 +118,10 @@ def Join_ride(rideid):
 def deleteride(rideid):
 	requests.post("http://34.238.33.155:80/api/v1/db/write", data=json.dumps({"ORIGIN":"RIDE", "COMMAND":"ADD_REQUEST_COUNT"}))
 	if rideid == '' or rideid == None:
-		return make_response('',400)
+		return make_response('RideID is empty.',400)
 
 	if requests.get("http://34.238.33.155:80/api/v1/db/read", params={"ORIGIN":"RIDE", "COMMAND":"EXISTS", "FIELD":"_id", "VALUE":rideid, "COLLECTION":"Rides"}).json()["count"] == 0:
-		return make_response('',400)
+		return make_response('There are no rides with given ride-id.',400)
 
 	requests.post("http://34.238.33.155:80/api/v1/db/write", data=json.dumps({"ORIGIN":"RIDE", "COMMAND":"DELETE", "FIELD":"_id", "VALUE":int(rideid), "COLLECTION":"Rides"}))
 	return make_response('',200)
